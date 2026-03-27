8f2d6d40924665954ffb843c9c27a7d8b5aa0e8b
CoderFish1 <shreypandey22005@gmail.com>
2026-03-27 12:12:38 +0530

    loading and pushing for new work

--- components/AddProdForm.jsx
+++ components/AddProdForm.jsx
@@ -2,14 +2,19 @@
 import React from 'react'
 import { useState } from 'react';
 import { Input } from './ui/input';
+import { Button } from './ui/button';
+import { Loader2 } from 'lucide-react';
 
 export const AddProdForm = ({user}) => {
  
   const [url, setUrl] = useState('');
-  const [loading, setloading] = useState('');
+  const [loading, setloading] = useState(false);
+
+  const handleSubmit = async (e) => {};
 
   return (
-    <form className='mx-auto w-full max-w-3xl'>
+    <>
+    <form onSubmit={handleSubmit} className='mx-auto w-full max-w-3xl'>
         <div className='flex flex-col sm:flex-row gap-3'
         >
             <Input
@@ -21,7 +26,24 @@ export const AddProdForm = ({user}) => {
             disabled ={loading}
             className='h-12 text-base mx-auto w-3xl border-3 hover:border-4'
             />
+
+            <Button className= 'mx-auto h-12 bg-emerald-800 hover:bg-emerald-700 hover:p-3 '
+            type="submit"
+            disabled={loading}
+            >
+                {loading ? (
+                    <>
+                    <Loader2 className='h-12 m-4 animate-spain'/>
+                    Adding...
+                    </> 
+                    ) : (
+                        "Track Price"
+                )}
+            </Button>
         </div>
     </form>
+
+    {/* authentication model */}
+    </>
   )
 }
